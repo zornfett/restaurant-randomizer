14c78da4ef7228f9b341541f75bb60f0e1ab7acf
Rob <rob@CRACs-MacBook-Air.local>
2018-12-13 11:43:44 -0800

    display txt onclick and victory anim

--- js/lunch-picker.js
+++ js/lunch-picker.js
@@ -1,14 +1,6 @@
-window.onload = function() {
-	console.log('loadie');
-
-};
-
 document.getElementById('submatron').onclick = function arrayStuffs() {
-	// debugger;
 	console.log('clicked');
-
-	var db = new Array('Stack1','Stack2','Stack16','Stack17','Stack13','Stack14','Stack15','Stack6','Stack7','Stack8','Stack9','Stack10');
-
+	// add restaurants here!
 	var setzt = new Array(
 		{
 			"id":1,
@@ -33,74 +25,27 @@ document.getElementById('submatron').onclick = function arrayStuffs() {
 			"name":"Open Face",
 			"phone":"855.676.3223",
 			"menu":"https://openfacefoodshop.com/menu/"
-		},
-		{
-			"id":666,
-			"name":"XXXX",
-			"phone":"XXXX",
-			"menu":"XXXX"
 		}
+		
+		// {
+		// 	"id":666,
+		// 	"name":"XXXX",
+		// 	"phone":"XXXX",
+		// 	"menu":"XXXX"
+		// }
 	);
-
+	// spin the wheel / display!
 	var i;
-	// for (i=0;i<db.length;i++){
-	// 	var restaurants = db[Math.floor(Math.random() * db.length)];
-	// 	document.getElementById('displayHere').innerText = restaurants;
-	// }
 	for (i=0;i<setzt.length;i++){
-		var restaurants = setzt[Math.floor(Math.random() * setzt.length)];
-		document.getElementById('displayHere').innerText = restaurants;
+		var restaurant = setzt[Math.floor(Math.random() * setzt.length)];
+		// var theWinner = restaurant['name'] + '\n' + restaurant['phone']  + '\n' + restaurant['menu'];
+		var name = restaurant['name'];
+		var phone = restaurant['phone'];
+		var menu = restaurant['menu'];
+		document.getElementById('displayHere').innerHTML = '<span>' + name + '</span><br>' + phone + '<br><a href="' + menu + '">' + menu + '</a>';
 	}
+	// snack storm!
+	var snacks = document.getElementById('snacks');
+	snacks.setAttribute("style", "display:block;");
+
 };
-
-
-/*
-FOR FORM SUBMIT:
-
-// https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onclick
-function initThings() {
-	console.log('loadie');
-	var butt = document.getElementById('submatron');
-	butt.onclick = collectFoodInfo();
-};
-
-
-// put field info into array - https://stackoverflow.com/questions/47078498/javascript-add-value-from-input-box-to-array/47078563
-var foodInfo = [];
-function collectFoodInfo() {
-	console.log('clicksauce');
-
-	fieldData = document.getElementsByClassName('field-data').value;
-	foodInfo.push(fieldData);
-	// console.log(foodInfo);
-	return false; // whoooooaa
-};
-
-
-DISCARD:
-window.onload = function() {
-	console.log('loadie');
-	// onclick run collectFoodInfo - https://stackoverflow.com/questions/21477717/how-to-call-a-js-function-using-onclick-event/21477793
-	function arrayPacker() {
-		document.geElementById('submatron').onclick {
-			console.log('clicksauce');
-			collectFoodInfo();
-		};
-	};
-};
-
-function arrayPacker2() {
-	document.geElementById('submatron').onclick {
-		console.log('clicksauce2');
-		collectFoodInfo();
-	}
-};
-
-// function initThings() {
-// 	console.log('loadie');
-// 	// var butt = document.getElementById('submatron');
-// 	// butt.onclick = arrayStuffs();
-
-// };
-
-*/
